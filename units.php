b31c6310dceb6463fd8e04cfdd940b38712d5c1a
Matt Thompson <matthew.thompson.a@gmail.com>
2016-08-21 14:13:07 -0400

    conversions in PHP

---
+++ units.php
@@ -0,0 +1,16 @@
+<?php
+//Conversions
+$lb_to_kg = 0.453592;
+$mile_to_km = 1.60934;
+
+$pounds = 250;
+$kilograms = $pounds * $lb_to_kg;
+
+echo "Weight:", $pounds, "lbs = ", $kilograms, " kg";
+
+$miles = 42;
+$km = $miles * $mile_to_km;
+
+echo "  ", $miles, "miles = ", $km, "km";
+
+?>
